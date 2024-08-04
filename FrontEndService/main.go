aaae1ef947e3d8bd4ad960d3a8f41268a4413bc7
NaokiKioko <DamienKrock@gmail.com>
2024-08-04 12:55:09 -0600

    Attempting

---
+++ FrontEndService/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	http.HandleFunc("/", handleIndex) // only file with headers/footers
+
+	http.ListenAndServe(":3000", nil)
+}
+
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "index.html", nil)
+}
+
+func renderTemplate(w http.ResponseWriter, templateName string, data interface{}) {
+	t, err := template.ParseFiles("templates/" + templateName)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	err = t.Execute(w, data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
