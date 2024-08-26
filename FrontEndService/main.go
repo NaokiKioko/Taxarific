d1ddb5f21e4ce49fb327b4272c255cc869339357
NaokiKioko <DamienKrock@gmail.com>
2024-08-26 00:20:39 -0600

    Desperatly trying

--- FrontEndService/main.go
+++ FrontEndService/main.go
@@ -3,21 +3,18 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"html/template"
-	"io"
 	"net/http"
-	"strings"
-	"time"
 
 	openapi_types "github.com/oapi-codegen/runtime/types"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/gin-gonic/gin"
-	"golang.org/x/crypto/bcrypt"
+	// "golang.org/x/crypto/bcrypt"
 )
 
 var usersBackendURl string = "http://backend-users-api:8080"
+
 var domainName string = "Taxarific.com"
 
 func main() {
@@ -26,13 +23,15 @@ func main() {
 	// Define routes
 	router.GET("/", handleIndex)
 	router.GET("/tax", handleTax)
+	
 	router.GET("/start", handleStart)
 	router.GET("/quiz", handleQuiz)
+
 	router.GET("/login", handleLogin)
 	router.POST("/login", handleLoginPost)
+
 	router.GET("/signup", handleSignup)
 	router.POST("/signup", handleSignupPost)
-	router.GET("/profilenav", handleProfileNav)
 
 	// Start server
 	router.Run(":3000")
@@ -40,6 +39,14 @@ func main() {
 }
 
 func handleIndex(c *gin.Context) {
+	jwt, err := c.Cookie("JWT")
+	if err != nil {
+		jwt = ""
+	}
+	if jwt != "" {
+		renderTemplate(c, "index/user.html", nil)
+		return
+	}
 	renderTemplate(c, "index/guest.html", nil)
 }
 
@@ -60,39 +67,49 @@ func handleLogin(c *gin.Context) {
 }
 
 func handleLoginPost(c *gin.Context) {
-	email := c.PostForm("email")
-	password := c.PostForm("password")
-
-	body := strings.NewReader(`{"username":"` + email + `","password":"` + password + `"}`)
-
-	res, err := sendReqstforJWT(c, usersBackendURl+"/login", body)
+	jwt, err := c.Cookie("JWT")
 	if err != nil {
+		jwt = ""
+	}
+	if jwt != "" {
+		c.JSON(http.StatusOK, gin.H{"token": jwt})
 		return
 	}
 
-	c.SetCookie("jwt", res.Token, 3600, "/", domainName, false, true)
-
-	// Respond with the JWT token
-	response := map[string]string{"token": res.Token}
-	c.JSON(http.StatusOK, response)
+	LoginRequest := LoginRequest{
+		Username: c.PostForm("email"),
+		Password: c.PostForm("password"),
+	}
+	JWTResponse := JWTResponse{}
+	err = SendRequest("GET", LoginRequest, usersBackendURl+"/user", &JWTResponse, "")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send request"})
+		return
+	}
+	c.SetCookie("JWT", "Bearer "+JWTResponse.Token, 3600, "/", domainName, false, true)
+	c.JSON(http.StatusOK, gin.H{"token": JWTResponse.Token})
 }
 
 type User struct {
-	Address   *string             `json:"address,omitempty"`
-	City      *string             `json:"city,omitempty"`
-	CreatedAt *time.Time          `json:"created_at,omitempty"`
-	Email     openapi_types.Email `json:"email"`
-	Id        primitive.ObjectID  `bson:"_id,omitempty" json:"id"`
-	Name      string              `json:"name"`
-	Password  string              `json:"omitempty"`
-	Phone     *string             `json:"phone,omitempty"`
-	State     *string             `json:"state,omitempty"`
-	UpdatedAt *time.Time          `json:"updated_at,omitempty"`
-	Zip       *string             `json:"zip,omitempty"`
+	Address  string              `json:"address,omitempty"`
+	City     string              `json:"city,omitempty"`
+	Email    openapi_types.Email `json:"email"`
+	Name     string              `json:"name"`
+	Password string              `json:"password"`
+	Phone    string              `json:"phone,omitempty"`
+	State    string              `json:"state,omitempty"`
+	Zip      string              `json:"zip,omitempty"`
+}
+type JWTResponse struct {
+	Token string `json:"token"`
+}
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func handleSignupPost(c *gin.Context) {
-	id := primitive.NewObjectID()
+	// id := primitive.NewObjectID()
 	email := c.PostForm("email")
 	password := c.PostForm("password")
 	name := c.PostForm("name")
@@ -109,32 +126,31 @@ func handleSignupPost(c *gin.Context) {
 	}
 
 	requestBody := User{
-		Id:       id,
-		Address:  &address,
-		City:     &city,
+		Address:  address,
+		City:     city,
 		Email:    openapi_types.Email(email),
 		Name:     name,
 		Password: hashedPassword,
-		Phone:    &phone,
-		State:    &state,
-		Zip:      &zip,
+		Phone:    phone,
+		State:    state,
+		Zip:      zip,
 	}
 
-	body, err := userToReader(requestBody)
+	fmt.Println(requestBody)
+
+	JWTResponse := JWTResponse{}
+	err = SendRequest("POST", requestBody, usersBackendURl+"/user", &JWTResponse, "")
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to marshal request body"})
+		println("Failed to send request")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send request"})
 		return
 	}
 
-	res, err := sendReqstforJWT(c, usersBackendURl+"/signup", body)
-	if err != nil {
-		return
-	}
-	c.SetCookie("jwt", res.Token, 3600, "/", domainName, false, true)
+	println("JWTResponse Token:")
+	println(JWTResponse.Token)
 
-	// Respond with the JWT token
-	response := map[string]string{"token": res.Token}
-	c.JSON(http.StatusOK, response)
+	c.SetCookie("JWT", "Bearer "+JWTResponse.Token, 3600, "/", domainName, false, true)
+	c.JSON(http.StatusOK, gin.H{"token": JWTResponse.Token})
 }
 
 func handleSignup(c *gin.Context) {
@@ -158,54 +174,48 @@ func renderTemplate(c *gin.Context, templateName string, data interface{}) {
 	}
 }
 
-func userToReader(user User) (io.Reader, error) {
-	// Convert the User struct to JSON
-	jsonData, err := json.Marshal(user)
+func SendRequest(httpverb string, data interface{}, url string, responseObj interface{}, authHeader string) error {
+	// Marshal the request data into JSON
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		return nil, err
+		return fmt.Errorf("failed to marshal data: %w", err)
 	}
 
-	// Return the JSON as an io.Reader
-	return bytes.NewReader(jsonData), nil
-}
-
-type JWTResponse struct {
-	Token string `json:"token"`
-}
-
-func sendReqstforJWT(c *gin.Context, url string, body io.Reader) (JWTResponse, error) {
-	req, err := http.NewRequest("POST", url, body)
+	// Create a new HTTP POST request
+	req, err := http.NewRequest(httpverb, url, bytes.NewBuffer(jsonData))
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
-		return JWTResponse{}, errors.New("failed to create request")
+		println(err)
+		return fmt.Errorf("failed to create request: %w", err)
 	}
-
 	req.Header.Set("Content-Type", "application/json")
-	res, err := http.DefaultClient.Do(req)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to execute request"})
-		return JWTResponse{}, errors.New("failed to execute request")
+	if authHeader != "" {
+		req.Header.Set("Authorization", authHeader)
 	}
-	defer res.Body.Close()
-
-	resBody, err := io.ReadAll(res.Body)
+	// Perform the HTTP request
+	client := &http.Client{}
+	println("Sending request to backend")
+	resp, err := client.Do(req)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to read response body"})
-		return JWTResponse{}, errors.New("failed to read response body")
+		return fmt.Errorf("failed to send request: %w", err)
 	}
-
-	// Parse the response body into JWTResponse struct
-	var jwtResponse JWTResponse
-	err = json.Unmarshal(resBody, &jwtResponse)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to parse JSON response"})
-		return JWTResponse{}, errors.New("failed to parse JSON response")
+	
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to send request: %w", err)
 	}
 
-	return jwtResponse, nil
+	defer resp.Body.Close()
+
+	// Decode the response body into the response object
+	err = json.NewDecoder(resp.Body).Decode(responseObj)
+	if err != nil {
+		println("Failed to decode response")
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+	return nil
 }
 
 func HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
-	return string(bytes), err
+	// bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	// return string(bytes), err
+	return password, nil
 }
